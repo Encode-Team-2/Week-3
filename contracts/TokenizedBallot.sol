1c9432f4b1c320f48b26b9d3df4f9a2a1f69d9a7
Demur Nodia <demur.nodia@gmail.com>
2023-08-12 09:51:21 +0400

    Initial commit

---
+++ contracts/TokenizedBallot.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+interface IMyToken {
+    function getPastVotes(address, uint256) external view returns (uint256);
+}
+
+contract TokenizedBallot {
+    struct Proposal {
+        bytes32 name;
+        uint voteCount;
+    }
+    Proposal[] public proposals;
+    address public tokenContract;
+    uint256 public targetBlockNumber;
+
+    constructor(
+        bytes32[] memory proposalNames,
+        address _tokenContract,
+        uint _targetTimePoint
+    ) {
+        tokenContract = _tokenContract;
+        targetBlockNumber = _targetTimePoint;
+
+        for (uint i = 0; i < proposalNames.length; i++) {
+            proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
+        }
+    }
+
+    function vote(uint proposal, uint amount) external {
+        // require the voting power to be highet than amount
+        // update the proposal vote count
+    }
+
+    function winningProposal() public view returns (uint winningProposal_) {
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+
+    function winnerName() external view returns (bytes32 winnerName_) {
+        winnerName_ = proposals[winningProposal()].name;
+    }
+}
